a73ff4cfaf443737aacf631c6eab5f12a36f3873
leif5672 <studium@leif-lebender.de>
2024-10-19 16:59:15 +0200

    Cleanup FanIn/FanOut search

--- C++ exercises/FanIns.cpp
+++ C++ exercises/FanIns.cpp
@@ -4,94 +4,83 @@
 
 //predef
 void sucheFanIns(std::vector<int>);
+void sucheFanIns2(int);
 void sucheFanOuts(int);
 
 //glob vars
 std::vector<int> fanIns;
 std::vector<int> fanOuts;
 
- //        nodes                   0, 1, 2, ..., 10   
+//        nodes                   0, 1, 2, ..., 10   
 std::vector<std::vector<int>> G {{4, 8, 5}, {9, 10}, {3, 1}, {9, 5}, {}, {4, 6}, {}, {3, 1, 8}, {9, 6}, {10, 6}, {}}; 
 
-int main ()
-{
+int main (){
+    int knoten = 9;
 
-             
-//std::vector<std::vector<int>>::iterator Knoten = 2;
-int Knoten = 6;
-
-std::vector<int> KnotenIns = G[Knoten];
-
-//sucheFanIns(G[Knoten]);
-
-sucheFanOuts(Knoten);
-
-for (int knoten : fanIns) {
-    std::cout << knoten << " ";
-}
-for (int knoten : fanOuts) {
-    std::cout << knoten << " ";
-}
-return 0;
+    //sucheFanIns(G[knoten]);
+    sucheFanIns2(knoten);
+    sucheFanOuts(knoten);
+        
+    // FanIns ausgeben
+    std::cout << "FanIns von Knoten " << knoten << " " << std::endl;
+    for (int fan : fanIns) {
+        std::cout << fan << " ";
+    }
+    
+    // FanOuts ausgeben
+    std::cout << "\nFanOuts von Knoten " << knoten << " " << std::endl;
+    for (int fan : fanOuts) {
+        std::cout << fan << " ";
+    }
+    return 0;
 }
 
 void sucheFanOuts (int suchknoten){
-
     int knotenNr = 0;
     for (std::vector<int> gKnoten : G){
-
         if (std::find(gKnoten.begin(), gKnoten.end(), suchknoten) != gKnoten.end()){                
-            
-          //  for (int gefundenerKnoten : gKnoten){
-                
                 if (std::find(fanOuts.begin(), fanOuts.end(), knotenNr) == fanOuts.end()){
                     fanOuts.push_back(knotenNr);
                     sucheFanOuts(knotenNr);
                 }
-           // }
         }
         knotenNr++;
     }
-    
-
 }
 
 void sucheFanIns2 (int suchknoten){
-    
     for (int gefundenerKnoten : G[suchknoten]){
-        if (std::find(fanOuts.begin(), fanOuts.end(), gefundenerKnoten) == fanOuts.end()){
-            fanOuts.push_back(gefundenerKnoten);
-            sucheFanOuts(gefundenerKnoten);
+        if (std::find(fanIns.begin(), fanIns.end(), gefundenerKnoten) == fanIns.end()){
+            fanIns.push_back(gefundenerKnoten);
+            sucheFanIns2(gefundenerKnoten);
         }
     }
 }
 
 void sucheFanIns (std::vector<int> suchknoten){
     // int i = 0;
- // do
-    // gefundenerKnoten = Suchknoten[i]
-    
-    // Wenn der gefundene Knoten nicht in FanIns  ist,
-        // füge den gefundenen Knoten hinzu
-        // wenn der Suchknoten  nicht leer ist
-           //suche(gefundenerKnoten)
+    // do
+        // gefundenerKnoten = Suchknoten[i]
+        
+        // Wenn der gefundene Knoten nicht in FanIns  ist,
+            // füge den gefundenen Knoten hinzu
+            // wenn der Suchknoten  nicht leer ist
+               //suche(gefundenerKnoten)
         
     // while nicht an der letzten stelle des Suchknotens i != Suchknoten.end ; i++ )  gehe eine stelle des Suchn´knotens weiter suche(gefundenerKnoten = Suchknoten[i+1])
-    
         // break
-int suchknotenstelle = 0;
 
-do {
-    int gefundenerKnoten = suchknoten[suchknotenstelle];
+    int suchknotenstelle = 0;
     
-    if (std::find(fanIns.begin(), fanIns.end(), gefundenerKnoten) == fanIns.end()){ // wenn der gefundene knoten nicht in fanIns ist
-        fanIns.push_back(gefundenerKnoten); // füge ihn hinzu
-        if (!G[gefundenerKnoten].empty()) //wenn der gefundene knoten nicht leer ist
-        sucheFanIns(G[gefundenerKnoten]); // hier passiert die REKURSION
-    }
-    
-    suchknotenstelle++;
-} while (suchknotenstelle < suchknoten.size());
-
-   
+    do {
+        int gefundenerKnoten = suchknoten[suchknotenstelle];
+        
+        if (std::find(fanIns.begin(), fanIns.end(), gefundenerKnoten) == fanIns.end()){ // wenn der gefundene knoten nicht in fanIns ist
+            fanIns.push_back(gefundenerKnoten); // füge ihn hinzu
+            if (!G[gefundenerKnoten].empty()) //wenn der gefundene knoten nicht leer ist
+                sucheFanIns(G[gefundenerKnoten]); // hier passiert die REKURSION
+        }
+        
+        suchknotenstelle++;
+    } while (suchknotenstelle < suchknoten.size());
 }
